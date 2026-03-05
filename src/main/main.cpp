e28ac06676d77fec8ab84411be9f64c1d6233fa9
15446-rus75 <vladabramov06@yandex.ru>
2026-03-05 22:00:52 +0300

    ya CG fix

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    TaskPlanner w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+  TaskPlanner w;
+  w.show();
+  return a.exec();
 }
