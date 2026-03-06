121d5bdf1eba76a48fe57f1db6a20cfc39b06317
15446-rus75 <vladabramov06@yandex.ru>
2026-03-06 13:59:37 +0300

    draft for Controller slots

--- src/controller/icontroller.hpp
+++ src/controller/icontroller.hpp
@@ -10,11 +10,20 @@ namespace controller
   class IController: public QObject
   {
     Q_OBJECT
+
   public:
     virtual ~IController() = default;
-    virtual void setStorage(storage::IStorage* storage) = 0;
-    virtual void setView(view::IView* view) = 0;
+    virtual void setStorage(storage::IStorage *storage) = 0;
+    virtual void setView(view::IView *view) = 0;
     virtual void start() = 0;
+
+  private slots:
+    void handleTaskAddRequest(const storage::Task &task);
+    void handleDateSelection(const QDate &date);
+    //НЕ СКОМПИЛИРУЕТСЯ, потому что нет определений класса Task
+
+  private:
+    bool validateTask(const storage::Task &task);
   };
 }
 
